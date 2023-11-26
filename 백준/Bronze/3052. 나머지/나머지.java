e513bc5ca2463da03d1272e4b9483273ea5b52a6
jisoo0609 <71012165+jisoo0609@users.noreply.github.com>
2023-11-26 20:26:26 +0900

    [Bronze II] Title: 나머지, Time: 212 ms, Memory: 17616 KB -BaekjoonHub

---
+++ 백준/Bronze/3052. 나머지/나머지.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int[] arr = new int[10];
+
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = sc.nextInt() % 42;
+		}
+
+		int count = 0;
+		boolean bl;
+		for (int i = 0; i < arr.length; i++) {
+			bl = false;
+
+			for (int j = i + 1; j < arr.length; j++) {
+				if (arr[i] == arr[j]) {
+					bl = true;
+					break;
+				}
+			}
+			if (bl == false) {
+				count++;
+			}
+		}
+		System.out.print(count);
+	}
+}
