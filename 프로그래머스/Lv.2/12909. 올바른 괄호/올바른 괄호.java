cc5a799c2084588c2fef1f3949de74e2a6f0d101
jisoo0609 <71012165+jisoo0609@users.noreply.github.com>
2024-01-13 22:14:49 +0900

    [level Lv.2] Title: 올바른 괄호, Time: 17.32 ms, Memory: 53.2 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.2/12909. 올바른 괄호/올바른 괄호.java
@@ -0,0 +1,23 @@
+import java.util.Stack;
+
+class Solution {
+    boolean solution(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char next = s.charAt(i);
+
+            if (next == '(') {
+                stack.push(next);
+            } else {
+                if (stack.isEmpty()) {
+                    return false;
+                } else {
+                    stack.pop();
+                }
+            }
+        }
+
+        return stack.isEmpty();
+    }
+}
