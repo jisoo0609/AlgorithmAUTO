1298722b7f590fc2a506f07917ccb93aac9a2bdd
jisoo0609 <71012165+jisoo0609@users.noreply.github.com>
2024-05-09 22:03:31 +0900

    [Silver I] Title: 그림, Time: 572 ms, Memory: 57264 KB -BaekjoonHub

--- 백준/Silver/1926. 그림/그림.java
+++ 백준/Silver/1926. 그림/그림.java
@@ -8,39 +8,40 @@ import java.util.StringTokenizer;
 
 public class Main {
     // 시작 지점
-    private static final int[] dx = {-1, 1, 0, 0};  // 좌 우 하 상
-    private static final int[] dy = {0, 0, -1, 1};  // 좌 우 하 상
-    public static void main(String[] args) throws IOException {
+    private static final int[] dx = {0, 0, -1, 1};  // 상 하 좌 우
+    private static final int[] dy = {-1, 1, 0, 0};  // 상 하 좌 우
 
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         String[] input = br.readLine().split(" ");
-        // 세로 크기
         int n = Integer.parseInt(input[0]);
-        // 가로 크기
         int m = Integer.parseInt(input[1]);
 
-        // 그림의 정보 입력받음
-        int[][] info = new int[n][m];
+        // 그림 입력받음
+        int[][] picture = new int[n][m];
         for (int i = 0; i < n; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine(), " ");
             for (int j = 0; j < m; j++) {
-                info[i][j] = Integer.parseInt(st.nextToken());
+                picture[i][j] = Integer.parseInt(st.nextToken());
             }
         }
+
         // 그림의 수
         int count = 0;
-        // 넓이
+
+        // 가장 넓은 그림의 넓이
         int max = 0;
-       
+
+        // BFS
         Queue<int[]> toVisit = new LinkedList<>();
         boolean[][] visited = new boolean[n][m];
 
-        // BFS
-        for (int i = 0; i <n; i++) {
+        for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-                // 시작 지점이 0이거나 continue
-                if (info[i][j] == 0 || visited[i][j]) continue;
+                // 시작 지점이 0이거나 방문한 지점이면 continue
+                if (picture[i][j] == 0 || visited[i][j]) continue;
+
                 // 그림의 개수 증가
                 count++;
                 // 시작 지점
@@ -52,7 +53,8 @@ public class Main {
                     int[] next = toVisit.poll();
                     int nowY = next[0];
                     int nowX = next[1];
-                    // 방문했으면 스킵
+
+                    // 방믄한 경우 스킵
                     if (visited[nowY][nowX]) continue;
                     visited[nowY][nowX] = true;
                     area++;
@@ -62,10 +64,11 @@ public class Main {
                         int nextX = nowX + dx[k];
 
                         if (0 <= nextY && nextY < n && 0 <= nextX && nextX < m &&
-                                info[nextY][nextX] == 1 &&
-                                !visited[nextY][nextX]
-                        )
-                            toVisit.add(new int[] {nextY, nextX});
+                            picture[nextY][nextX] == 1 &&
+                            !visited[nextY][nextX])
+                        {
+                            toVisit.add(new int[]{nextY, nextX});
+                        }
                     }
                 }
                 max = Math.max(max, area);
