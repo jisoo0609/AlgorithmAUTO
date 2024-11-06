63109d79e5191bb3114a28f023ef4f8bddec6fb0
jisoo0609 <71012165+jisoo0609@users.noreply.github.com>
2024-11-06 21:54:02 +0900

    [level 2] Title: 의상, Time: 0.11 ms, Memory: 74.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -0,0 +1,30 @@
+
+import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public int solution(String[][] clothes) {
+        int answer = 1;
+        HashMap<String, List<String>> hashMap = new HashMap<>();
+        
+        for(int i=0; i<clothes.length; i++) {
+            List<String> list = new ArrayList<>();
+            String key = clothes[i][1];
+            String value = clothes[i][0];
+            
+            if(!hashMap.containsKey(key)) {
+                list.add(value);
+                hashMap.put(key, list);
+            } else {
+                hashMap.get(key).add(value);
+            }
+            
+        }
+        for(String key : hashMap.keySet()) {
+            answer *= hashMap.get(key).size()+1;
+        }
+        
+        return answer -1;
+    }
+}
