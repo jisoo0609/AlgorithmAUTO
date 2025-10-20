f45af08909679762817c8040ce7d18bf447cd18e
jisoo0609 <71012165+jisoo0609@users.noreply.github.com>
2025-10-20 19:17:32 +0900

    Time: 2 ms (50.11%), Space: 45.1 MB (26.53%) - LeetHub

---
+++ LeetCode/2942-find-words-containing-character/2942-find-words-containing-character.java
@@ -0,0 +1,14 @@
+class Solution {
+    public List<Integer> findWordsContaining(String[] words, char x) {
+        List<Integer> result = new ArrayList<>();
+
+        for (int i = 0; i < words.length; i++) {
+            String str = words[i];
+            if (str.contains(String.valueOf(x))) {
+                result.add(i);
+            }
+        }
+
+        return result;
+    }
+}
