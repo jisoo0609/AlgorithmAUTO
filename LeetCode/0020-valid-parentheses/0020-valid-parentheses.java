16d6af12cb4cce62e0d5b3ae2ca317614e27074f
jisoo0609 <71012165+jisoo0609@users.noreply.github.com>
2025-10-22 00:11:05 +0900

    Time: 2 ms (97.37%), Space: 42 MB (39.2%) - LeetHub

---
+++ LeetCode/0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,33 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+
+            switch(c) {
+                case '(' :
+                case '{' :
+                case '[' :
+                    stack.push(c);
+                    break;
+                case ')' :
+                    if (stack.isEmpty() || stack.pop() != '(') {
+                        return false;
+                    }
+                    break;
+                case '}' :
+                    if (stack.isEmpty() || stack.pop() != '{') {
+                        return false;
+                    }
+                    break;
+                case ']' :
+                    if (stack.isEmpty() || stack.pop() != '[') {
+                        return false;
+                    }
+                    break;
+            }
+        }
+        return stack.isEmpty();
+    }
+}
